0a3ae033ecc0fac8a62f7c9cf5c71ad4a54b2441
bluew <bluewww@users.noreply.github.com>
2018-10-28 20:41:50 +0100

    Set font via default-frame-alist, it's about 0.1 to 0.2 sec faster

--- init.el
+++ init.el
@@ -793,13 +793,8 @@ buffer is not visiting a file."
 	   auctex-latexmk anaconda-mode)))
  '(truncate-lines t))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil
-			 :strike-through nil :overline nil :underline nil :slant
-			 normal :weight normal :height 110 :width normal
-			 :foundry "unknown" :family "DejaVu Sans Mono")))))
 (put 'dired-find-alternate-file 'disabled nil)
+
+;; set default font
+(add-to-list 'default-frame-alist
+             '(font . "DejaVu Sans Mono-11"))
