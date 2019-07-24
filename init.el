234ba3ac403a1b926ecaeea214ff75ce2c346483
bluew <bluewww@users.noreply.github.com>
2019-07-24 02:55:26 +0200

    Enable tab in terminal emacs
    Since C-i is equivalent to tab in a terminal we have to disable the C-i
    evil binding to make org-mode bearable there.

--- init.el
+++ init.el
@@ -178,6 +178,9 @@ When you add a new element to the alist, keep in mind that you
   :init
   (setq evil-want-C-u-scroll t)
   (setq evil-want-integration nil)
+  (when (not (display-graphic-p))
+    (setq evil-want-C-i-jump nil))
+  ;(setq evil-want-minibuffer t)
   :config
   (evil-mode 1)
   (global-undo-tree-mode 1)
