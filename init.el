15231a6012db147b18d14e510a687882a6e964fe
bluew <bluewww@users.noreply.github.com>
2022-03-27 04:10:47 +0200

    Add llvm.org c-style

--- init.el
+++ init.el
@@ -1129,6 +1129,17 @@ window."
     (setq-default c-default-style "linux")
     (setq-default c-basic-offset 4)))
 
+;; Add a cc-mode style for editing LLVM C and C++ code
+(c-add-style "llvm.org"
+             '("gnu"
+	       (fill-column . 80)
+	       (c++-indent-level . 2)
+	       (c-basic-offset . 2)
+	       (indent-tabs-mode . nil)
+	       (c-offsets-alist . ((arglist-intro . ++)
+				   (innamespace . 0)
+				   (member-init-intro . ++)
+				   (statement-cont . llvm-lineup-statement)))))
 ;;; Stuff from customize
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
