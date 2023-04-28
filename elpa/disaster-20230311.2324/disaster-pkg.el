e40c3c2f00397b8dee3942230530123a5cbd167e
bluew <bluewww@users.noreply.github.com>
2023-04-28 17:37:07 +0200

    Bump package to 2023.04.28

---
+++ elpa/disaster-20230311.2324/disaster-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from disaster.el  -*- no-byte-compile: t -*-
+(define-package "disaster" "20230311.2324" "Disassemble C, C++ or Fortran code under cursor" '((emacs "27")) :commit "16bba9afb92aacf06c088c29ba47813b65a80d87" :authors '(("Justine Tunney" . "jtunney@gmail.com") ("Abdelhak Bougouffa" . "abougouffa@fedoraproject.org")) :maintainers '(("Abdelhak Bougouffa" . "abougouffa@fedoraproject.org")) :maintainer '("Abdelhak Bougouffa" . "abougouffa@fedoraproject.org") :keywords '("tools" "c") :url "https://github.com/jart/disaster")
--- elpa/loc-changes-20200722.1111/loc-changes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from loc-changes.el  -*- no-byte-compile: t -*-
-(define-package "loc-changes" "20200722.1111" "keep track of positions even after buffer changes" 'nil :commit "2db42905675e1740305f9884613bc48582601b8a" :authors '(("Rocky Bernstein" . "rocky@gnu.org")) :maintainer '("Rocky Bernstein" . "rocky@gnu.org") :url "https://github.com/rocky/emacs-loc-changes")
