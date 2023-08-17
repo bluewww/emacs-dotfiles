552e6fd629cb10d04dfee821d46a9c5aa7bdd3f5
bluew <bluewww@users.noreply.github.com>
2023-08-17 16:38:57 +0200

    Bump package to 2023.08.17

---
+++ elpa/clang-format-20230809.2200/clang-format-pkg.el
@@ -0,0 +1,7 @@
+(define-package "clang-format" "20230809.2200" "Format code using clang-format"
+  '((cl-lib "0.3"))
+  :commit "bce0bb4ca17ecb6e6927074920945cef2a78f5b6" :keywords
+  '("tools" "c"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/merlin-20230726.900/merlin-pkg.el
@@ -0,0 +1,14 @@
+(define-package "merlin" "20230726.900" "Mode for Merlin, an assistant for OCaml"
+  '((emacs "25.1"))
+  :commit "fd1bac38fcfe601094ce8e01895333666733e664" :authors
+  '(("Frédéric Bour <frederic.bour(_)lakaban.net>"))
+  :maintainers
+  '(("Frédéric Bour <frederic.bour(_)lakaban.net>"))
+  :maintainer
+  '("Frédéric Bour <frederic.bour(_)lakaban.net>")
+  :keywords
+  '("ocaml" "languages")
+  :url "https://github.com/ocaml/merlin")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
